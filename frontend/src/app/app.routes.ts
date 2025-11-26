a567ce163853389cca46693b46d9e598eceee662
Natchanon Vongvivitpattana <chhanon05@gmail.com>
2025-11-26 15:21:35 +0700

    home

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,15 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/User/home/home.component';
 
 export const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: '**', redirectTo: 'login' }
+  // { path: 'home', component: HomeComponent },
+
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', redirectTo: 'home' }
 ];
+//   { path: 'login', component: LoginComponent },
+//   { path: '', redirectTo: 'login', pathMatch: 'full' },
+//   { path: '**', redirectTo: 'login' }
+// ];
