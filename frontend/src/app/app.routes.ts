ca4e11f8f23da291995a5ee5b331bd031585adbb
Natchanon Vongvivitpattana <chhanon05@gmail.com>
2025-12-01 19:48:53 +0700

    Register

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,14 +1,20 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/User/home/home.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 export const routes: Routes = [
+  { path: 'register', component: RegisterComponent },
+  { path: '', redirectTo: 'register', pathMatch: 'full' },
+  { path: '**', redirectTo: 'register' }
+
+];
   // { path: 'home', component: HomeComponent },
 
-  { path: 'home', component: HomeComponent },
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: '**', redirectTo: 'home' }
-];
+//   { path: 'home', component: HomeComponent },
+//   { path: '', redirectTo: 'home', pathMatch: 'full' },
+//   { path: '**', redirectTo: 'home' }
+// ];
 //   { path: 'login', component: LoginComponent },
 //   { path: '', redirectTo: 'login', pathMatch: 'full' },
 //   { path: '**', redirectTo: 'login' }
--- frontend/src/app/pages/register/register.component.ts
+++ frontend/src/app/pages/register/register.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [FormsModule, NgIf],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss',
 })
 export class RegisterComponent {
+  Username: string = '';
+  Password: string = '';
+  errorMessage: string = '';
+
+  togglePasswordVisibility() { // แสดงรหัสผ่าน
+    const input = document.getElementById('passwordinput') as HTMLInputElement;
+    input.type = input.type === 'password' ? 'text' : 'password';
+  }
 
 }
