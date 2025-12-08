fc59c21a7b73ba1bc288751f7e7fc64cf1c6cb72
Natchanon Vongvivitpattana <chhanon05@gmail.com>
2025-12-08 20:06:43 +0700

    multiselect dropdown

--- frontend/src/app/pages/User/inventory/inventory.component.ts
+++ frontend/src/app/pages/User/inventory/inventory.component.ts
@@ -1,31 +1,41 @@
 import { Component } from '@angular/core';
 import { SidebarUserComponent } from "../../../component/sidebar-user/sidebar-user.component";
 import { TableModule } from 'primeng/table';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-inventory',
-  imports: [SidebarUserComponent,TableModule],
+  imports: [SidebarUserComponent,TableModule, MultiSelectModule,FormsModule],
   templateUrl: './inventory.component.html',
   styleUrl: './inventory.component.scss',
 })
 export class InventoryComponent {
 
     products = [
-    { code: 'P001', name: 'Pen', category: 'Stationery', quantity: 50 },
-    { code: 'P002', name: 'Book', category: 'Stationery', quantity: 120 },
-    { code: 'P003', name: 'Book', category: 'box', quantity: 5 },
-   { code: 'P001', name: 'Pen', category: 'Stationery', quantity: 50 },
-    { code: 'P002', name: 'Book', category: 'Stationery', quantity: 120 },
-    { code: 'P003', name: 'Book', category: 'box', quantity: 5 },
-    { code: 'P001', name: 'Pen', category: 'Stationery', quantity: 50 },
-    { code: 'P002', name: 'Book', category: 'Stationery', quantity: 120 },
-    { code: 'P003', name: 'Book', category: 'box', quantity: 5 },
-     { code: 'P003', name: 'Book', category: 'box', quantity: 5 },
-      { code: 'P002', name: 'Book', category: 'Stationery', quantity: 120 },
-    { code: 'P003', name: 'Book', category: 'box', quantity: 5 },
-     { code: 'P003', name: 'Book', category: 'box', quantity: 5 }
-   
-   
-   
+    { code: 'P001', name: 'Pen', category: 'Stationery', quantity: 50, price: 10},
+    { code: 'P002', name: 'Book', category: 'Stationery', quantity: 120, price: 30 },
+    { code: 'P003', name: 'Book', category: 'Box', quantity: 5 ,price: 20}
+
   ];
+
+    categoryOptions = [
+    { label: 'Stationery', value: 'Stationery' },
+    { label: 'Box', value: 'Box' }
+  ];
+
+  selectedCategories: string[] = [];
+
+  filteredProducts = [...this.products];
+
+  filterProducts() {
+    if (this.selectedCategories.length === 0) {
+      this.filteredProducts = [...this.products];
+      return;
+    }
+
+    this.filteredProducts = this.products.filter(p =>
+      this.selectedCategories.includes(p.category)
+    );
+  }
 }
