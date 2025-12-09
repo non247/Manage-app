7d6348de23be3b4dd11d79de606f32c77ea00e39
titaks35 <turkungr35@gmail.com>
2025-12-09 13:45:52 +0700

    fix: login button color and format code.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,21 +2,46 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { DashboardComponent } from './pages/User/Dashboard/Dashboard.component';
-import { InventoryComponent } from './pages/User/inventory/inventory.component';
 import { HistoryComponent } from './pages/User/history/history.component';
+import { InventoryComponent } from './pages/User/inventory/inventory.component';
 import { SettingComponent } from './pages/User/setting/setting.component';
 
 export const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'inventory', component: InventoryComponent},
-  { path: 'history', component: HistoryComponent},
-  { path: 'setting',component: SettingComponent },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+  },
+  {
+    path: 'inventory',
+    component: InventoryComponent,
+  },
+  {
+    path: 'history',
+    component: HistoryComponent,
+  },
+  {
+    path: 'setting',
+    component: SettingComponent,
+  },
 
   // หน้าเริ่มต้น
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
 
   // ถ้าพิมพ์ path ที่ไม่มีจริง ให้กลับไปหน้า login
-  { path: '**', redirectTo: 'login' }
+  {
+    path: '**',
+    redirectTo: 'login',
+  },
 ];
