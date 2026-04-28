8600bf5642569efff1ae0e60b2d402815171ad65
Natchanon Vongvivitpattana <chhanon05@gmail.com>
2026-04-28 14:20:14 +0700

    update database

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,12 +1,21 @@
 const { Pool } = require('pg');
 
-const pool = new Pool({
-  host: 'localhost',
-  user: 'postgres',
-  port: 5432,
-  password: '1234',
-  database: 'CompanyDB',
-});
+const isProduction = process.env.DATABASE_URL;
+
+const pool = new Pool(
+  isProduction
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false },
+      }
+    : {
+        host: 'localhost',
+        user: 'postgres',
+        port: 5432,
+        password: '1234',
+        database: 'CompanyDB',
+      }
+);
 
 pool.on('connect', () => {
   console.log('PostgreSQL connected');
