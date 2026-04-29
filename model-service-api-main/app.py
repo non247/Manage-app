525f4a4e1b34c2742be640c12669289c1a296deb
Natchanon Vongvivitpattana <chhanon05@gmail.com>
2026-04-29 20:15:05 +0700

    "fix render port for model service"

--- model-service-api-main/app.py
+++ model-service-api-main/app.py
@@ -349,6 +349,7 @@ def predict_soldperday():
 
 
 if __name__ == "__main__":
-    # For local development: app.run(debug=True, port=5000)
-    # For deployment, use a production-ready WSGI server like Gunicorn.
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    import os
+
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
