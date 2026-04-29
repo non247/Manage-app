540dcd2a4e163f661d7b460bfc3ade3509838bef
Natchanon Vongvivitpattana <chhanon05@gmail.com>
2026-04-29 14:04:37 +0700

    environment frontend

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://manage-app-5koc.onrender.com/api'
+};
---
+++ frontend/src/environments/environments.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://manage-app-5koc.onrender.com/api'
+};
